92178cc44018d823074e6d36d732a8aa792de00a
mauriciortega07 <mauriciortega93@gmail.com>
2025-07-28 05:15:24 -0600

    archivoa adicionales

--- src/main/java/com/ebac/practica65ServiciosWebREST/controller/TelefonoController.java
+++ src/main/java/com/ebac/practica65ServiciosWebREST/controller/TelefonoController.java
@@ -6,7 +6,6 @@ import com.ebac.practica65ServiciosWebREST.service.TelefonoService;
 import com.ebac.practica65ServiciosWebREST.service.UsuarioService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
